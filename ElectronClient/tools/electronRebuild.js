acab0034a5fdd578f11eb64e5ec1640dc18ab0c1
genneko <genneko@users.noreply.github.com>
2020-05-31 15:07:07 +0900

    Revert "Remove a tweak for FreeBSD because it's no longer needed"
    This reverts commit e778d0ace08e8e44e31048c8824a1180ff5f2dbe.

--- ElectronClient/tools/electronRebuild.js
+++ ElectronClient/tools/electronRebuild.js
@@ -22,6 +22,10 @@ const isWindows = () => {
 	return process && process.platform === 'win32';
 };
 
+const isFreeBSD = () => {
+	return process && process.platform === 'freebsd';
+};
+
 async function main() {
 	// electron-rebuild --arch ia32 && electron-rebuild --arch x64
 
@@ -33,6 +37,9 @@ async function main() {
 	if (isWindows()) {
 		console.info(await execCommand([`"${exePath}"`, '--arch ia32'].join(' ')));
 		console.info(await execCommand([`"${exePath}"`, '--arch x64'].join(' ')));
+	} else if (isFreeBSD()) {
+		console.info(await execCommand([`"${exePath}"`, '-e /usr/local/bin', '--version `cat /usr/local/share/electron/version`'].join(' ')));
+
 	} else {
 		console.info(await execCommand([`"${exePath}"`].join(' ')));
 	}
